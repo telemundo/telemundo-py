8e58ad6377fc2dbf0a828709310869b2126e992c
Rodolfo Puig <Rodolfo.Puig@nbcuni.com>
2012-09-27 14:56:53 -0400

    Moving the packages to a sub-directory

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='TelemundoPy',
-    version='0.1',
+    version='0.1-dev',
     url='https://github.com/telemundo/telemundo-py',
     author='Rodolfo Puig',
     author_email='rodolfo.puig@nbcuni.com',
     description='Telemundo Python Libraries',
     keywords='Telemundo',
     license='MIT',
-    packages=find_packages()
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any'
 )
