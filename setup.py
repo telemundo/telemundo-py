ba3cb0581cd7f747431222051104b007374bfaf5
Rodolfo Puig <Rodolfo.Puig@nbcuni.com>
2012-09-27 14:47:49 -0400

    Creating setuptools bootstrap file for packaging purposes

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='TelemundoPy',
+    version='0.1',
+    url='https://github.com/telemundo/telemundo-py',
+    author='Rodolfo Puig',
+    author_email='rodolfo.puig@nbcuni.com',
+    description='Telemundo Python Libraries',
+    keywords='Telemundo'
+    license='MIT',
+    packages=find_packages()
+)
